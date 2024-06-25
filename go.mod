562fc130d09ad7d43d64dbbf3810db32ed26234b
JJ810 <hcls80205@gmail.com>
2024-06-25 10:04:51 -0400

    get env & set port

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vlad0831/rss_aggregator
+
+go 1.22.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	fmt.Println("PORT is set to", portString)
 }
