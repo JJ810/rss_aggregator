50a20c00654ca9498a91ea3915dafc04ae13efd7
JJ810 <hcls80205@gmail.com>
2024-06-26 16:08:14 -0400

    get user by api key

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/vlad0831/rss_aggregator/internal/auth"
 	"github.com/vlad0831/rss_aggregator/internal/database"
 )
 
@@ -33,6 +34,23 @@ func (apiCfg apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request
 	if err != nil {
 		respondWithError(w, 500, fmt.Sprintf("Error creating user: %v", err))
 		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 	}
 
 	respondWithJSON(w, 200, databaseUserToUser(user))
---
+++ internal/auth/auth.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// GetAPIKey extracts an API key from
+// the headers of an HTTP request
+// Example:
+// Authorization: ApiKey {insert apikey here}
+func GetAPIKey(headers http.Header) (string, error) {
+	val := headers.Get("Authorization")
+	if val == "" {
+		return "", errors.New("no API key provided")
+	}
+
+	vals := strings.Split(val, " ")
+	if len(vals) != 2 {
+		return "", errors.New("malformed auth header")
+	}
+
+	if vals[0] != "ApiKey" {
+		return "", errors.New("malformed first part of auth header")
+	}
+
+	return vals[1], nil
+}
